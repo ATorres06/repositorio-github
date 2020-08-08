0ec9417584d67f6c3bcbf9c7e2052d0702563aa4
ATorres06 <torresabilleira@gmail.com>
2020-08-08 03:08:41 -0300

    asdf

--- entrega0/js/products.js
+++ entrega0/js/products.js
@@ -1,6 +1,40 @@
+var listado = []
+function mostrar(array){
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let category = array[i];
+        htmlContentToAppend +=`
+        <div>
+        <img src=` + category.imgSrc + ` alt=` + category.description + `></img>
+        <h4>`+ category.name + `</h4>
+                        
+                    
+        <p>` + category.soldCount + ` artículos</p>
+    
+    <p>`+ category.description +  `</p>
+    <p>`+ category.currency + ``+ category.cost +  `</p>
+       
+        </div>
+        `
+        
+    }
+    document.getElementById("productos").innerHTML = htmlContentToAppend;
+}
+    
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+         mostrar(categoriesArray)
+        }
+    });
 });
+
